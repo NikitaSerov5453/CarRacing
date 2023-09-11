e25be56f710e66fde9ee8fd9dd28b5f14ff148fc
dante5453 <nikita.serov5453@gmail.com>
2023-09-11 19:57:45 +0300

    14

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -13,19 +13,75 @@ public class Car {
     }
 
     public void createCar() {
-        Body body = new Body();
-        Transmission transmission = new Transmission();
-        Wheel wheel = new Wheel();
-        Engine engine = new Engine();
-
-        System.out.println("Введите массу авто");
-        body.setMass(scanner.nextInt());
-        System.out.println("Введите название кузова");
-        body.setName(scanner.nextLine());
-        body.setCost(0);
-        engine.setName(scanner.nextLine());
-        engine.setPower(scanner.nextInt());
-        engine.setCost(0);
+        Controller controller = new Controller();
+        System.out.println("Выберете кузов");
+        System.out.println("1. Lada Priora");
+        System.out.println("2. Toyota Supra");
+        System.out.println("3. Nissan GTR 35");
+        switch (scanner.nextInt()) {
+            case 1:
+                body = controller.createBodyLadaPriora();
+                break;
+            case 2:
+                body = controller.createBodyToyotaSupra();
+                break;
+            case 3:
+                body = controller.createBodyNissanGTR35();
+                break;
+            default:
+                System.out.println("Ошибка");
+        }
+        System.out.println("Выберете двигатель");
+        System.out.println("1. 2JZ");
+        System.out.println("2. VR38DET");
+        System.out.println("3. 3110(");
+        switch (scanner.nextInt()) {
+            case 1:
+                engine = controller.createEngine2JZ();
+                break;
+            case 2:
+                engine = controller.createEngineVR38DET();
+                break;
+            case 3:
+                engine = controller.createEngine3110();
+                break;
+            default:
+                System.out.println("Ошибка");
+        }
+        System.out.println("Выберете КПП");
+        System.out.println("1. TTI");
+        System.out.println("2. Samsonas");
+        System.out.println("3. default");
+        switch (scanner.nextInt()) {
+            case 1:
+                transmission = controller.createTransmissionTTI();
+                break;
+            case 2:
+                transmission = controller.createTransmissionSamsonas();
+                break;
+            case 3:
+                transmission = controller.createTransmissionDeafult();
+                break;
+            default:
+                System.out.println("Ошибка");
+        }
+        System.out.println("Выберете колеса");
+        System.out.println("1. Ahiles");
+        System.out.println("2. Westlake");
+        System.out.println("3. Valina");
+        switch (scanner.nextInt()) {
+            case 1:
+                wheel = controller.createWheelAhiles();
+                break;
+            case 2:
+                wheel = controller.createWheelWestlake();
+                break;
+            case 3:
+                wheel = controller.createWheelValina();
+                break;
+            default:
+                System.out.println("Ошибка");
+        }
     }
 
     @Override
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
+        Car car1 = new Car();
+        Controller controller = new Controller();
+        car.createCar();
         System.out.println(car);
+        car1.createCar();
+        System.out.println(car1);
+        controller.carComparison(car, car1);
+
     }
 }
